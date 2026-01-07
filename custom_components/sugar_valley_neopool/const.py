9e5ae7683ab1dd47b33d6c0cc2b1a1f76fcb28c8
Alessandro Del Prete <alessandro.delprete@gmail.com>
2026-01-07 06:20:44 +0100

    chore: bump version to 0.2.11 for next development cycle

--- custom_components/sugar_valley_neopool/const.py
+++ custom_components/sugar_valley_neopool/const.py
@@ -9,7 +9,7 @@ from homeassistant.const import Platform
 # Integration identity
 DOMAIN: Final = "sugar_valley_neopool"
 NAME: Final = "Sugar Valley NeoPool"
-VERSION: Final = "0.2.10"
+VERSION: Final = "0.2.11"
 MANUFACTURER: Final = "Sugar Valley"
 MODEL: Final = "NeoPool Controller"
 ATTRIBUTION: Final = "by @alexdelprete"
